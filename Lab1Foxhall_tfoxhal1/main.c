a18cbb47b06b54666efe56f22d8611d21fd64dee
Taylor Foxhall <lxmachina@gmail.com>
2016-01-28 18:28:46 -0500

    lab class over

--- Lab1Foxhall_tfoxhal1/main.c
+++ Lab1Foxhall_tfoxhal1/main.c
@@ -1,7 +1,73 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h> // qsort, malloc, free...
+#include <unistd.h> // getopt
 
-int main(int argc, char *argv[]) {
-  printf("Hello world\n");
+typedef struct {
+  int mode; // 0 for sort, 1 for gen
+  int size; // Number of ints to read/gen
 
+  // gen mode options
+  int gen_min; // Minimum number
+  int gen_max; // Maximum number
+
+  // Seed option is set using srand
+
+  // Probably change type from char * to FILE *
+  char *input; // Input file
+  char *output; // Output file
+  char *count_out; // Output for counts
+} Option;
+
+Option *Option_new(int, char **);
+void Option_destroy(Option *);
+
+Option *Option_new(int argc, char **argv) {
+  char flag;
+
+  // Initialize new Option struct using argv and getopts
+  Option *option = malloc(sizeof(Option));
+  option->mode = 0; // sort mode by default
+
+  while ((flag = getopt(argc, argv, "ugn:m:M:s:i:o:c:")) != -1) {
+    switch (flag) {
+    case 'u':
+    case '?':
+      // Just kick out and let main print usage
+      Option_destroy(option);
+      return NULL;
+    case 'g':
+      option->mode = 1;
+      break;
+    case 'n':
+      option->size = 
+    default:
+      // Just kick out and let main print usage
+      Option_destroy(option);
+      return NULL;
+    }
+  }
+
+  return option;
+}
+
+void Option_destroy(Option *option) {
+  free(option);
+  option = NULL;
+}
+
+/* lab1 [-u] [-g] [-n <num-integers>] [-m <min-int>] [-M <max-int>] [-s <seed>]
+   [-i <input-file-name>] [-o <output-file-name>] [-c <count-file-name>] */
+int main(int argc, char **argv) {
+  Option *option = Option_new(argc, argv);
+
+  if (!option) {
+    fprintf(stderr, "Usage: lab1 [-u] [-g] [-n <num-integers>] [-m <min-int>] [-M <max-int>] [-s <seed>]"
+            "[-i <input-file-name>] [-o <output-file-name>] [-c <count-file-name>]");
+    abort();
+  }
+
+  // Cleanup
+  Option_destroy(option);
   return 0;
 }
